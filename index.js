7dd29592c2bc7f85f28664a523177a75eae712c0
Jason S. Jones <jsjones96@gmail.com>
2015-12-15 06:03:13 -0800

    Add methods to addVertex & addEdge with tests

--- index.js
+++ index.js
@@ -18,19 +18,28 @@
 
     }
 
+    Graph.prototype.addVertex = function (label) {
+        this.vertices.push(label);
+        this.adjacentList.set(label, []);
+    };
+
+    Graph.prototype.addEdge = function (v1, v2) {
+        if (this.adjacentList.has(v1) && this.adjacentList.has(v2)) {
+            this.adjacentList.get(v1).push(v2);
+            this.adjacentList.get(v2).push(v1);
+        }
+    };
+
     Graph.prototype.toString = function () {
         var str = '';
         for (var i = 0; i < this.vertices.length; i++) {
-            str  += this.vertices[i] + ': ';
+            str  += this.vertices[i] + ':';
             var connectedVerts = this.adjacentList.get(this.vertices[i]);
             for (var j = 0; j < connectedVerts.length; j++) {
-                str += connectedVerts[j] + ' ';
+                str += ' ' + connectedVerts[j];
             }
-            if (i !== this.vertices.length - 1) {
-                str += ', ';
-            }
+            str += '\n';
         }
-        str += '\n';
         return str;
     };
 
@@ -41,7 +50,9 @@
     }
 
     function main() {
-        var graph = new Graph(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']);
+        var graph = new Graph(['A', 'B', 'C', 'D']);
+        graph.addEdge('A', 'B');
+        graph.addEdge('A', 'C');
         console.log(graph.toString());
     }
 }());
--- test/graph.spec.js
+++ test/graph.spec.js
@@ -2,8 +2,45 @@ var expect = require('chai').expect;
 var Graph = require('../');
 
 describe('Graph data structure', function () {
-    it('instantiates a graph instance', function () {
+    it('instantiates a graph instance with no parameters', function () {
         var graph = new Graph();
         expect(graph).to.exist;
     });
+
+    it('instantiates a graph instance provided an arrary of vertices', function () {
+        var graph = new Graph(['A', 'B', 'C', 'D']);
+        expect(graph.vertices).to.be.Array;
+        expect(graph.vertices).to.have.length(4);
+    });
+
+    it('when instantiated creates a vertices Array', function () {
+        var graph = new Graph();
+        expect(graph.vertices).to.exist;
+        expect(graph.vertices).to.be.Array;
+    });
+
+    it('when instantiated creates an adjacency list', function () {
+        var graph = new Graph();
+        expect(graph.adjacentList).to.exist;
+    });
+
+    it('adds a vertex to the graph', function () {
+        var graph = new Graph();
+        graph.addVertex('A');
+        graph.addVertex('B');
+        expect(graph.vertices).to.have.length(2);
+    });
+
+    it('adds an edge between vertices', function () {
+        var graph = new Graph();
+        graph.addVertex('A');
+        graph.addVertex('B');
+        graph.addVertex('C');
+        graph.addEdge('A', 'B');
+        graph.addEdge('A', 'C');
+        expect(graph.adjacentList.get('A').indexOf('B') > -1).to.be.true;
+        expect(graph.adjacentList.get('A').indexOf('C') > -1).to.be.true;
+        expect(graph.adjacentList.get('B').indexOf('A') > -1).to.be.true;
+    });
+
 });
