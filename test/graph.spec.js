8059e9a56a468d3ee2256f58958217d4dc5b22ae
Jason Jones <jsjones96@gmail.com>
2015-12-21 05:20:52 +0000

    Refactor test suite and add test for BFS

--- test/graph.spec.js
+++ test/graph.spec.js
@@ -1,6 +1,45 @@
 var expect = require('chai').expect;
 var Graph = require('../');
 
+function getSimpleGraph() {
+    var g = new Graph();
+
+    g.addVertex('A');
+    g.addVertex('B');
+    g.addVertex('C');
+    g.addVertex('D');
+
+    g.addEdge('A', 'B');
+    g.addEdge('A', 'C');
+    g.addEdge('C', 'D');
+
+    return g;
+}
+
+function getComplexGraph() {
+    var g = new Graph();
+
+    g.addVertex('A');
+    g.addVertex('B');
+    g.addVertex('C');
+    g.addVertex('D');
+    g.addVertex('E');
+    g.addVertex('F');
+    g.addVertex('G');
+    g.addVertex('H');
+
+    g.addEdge('A', 'B');
+    g.addEdge('A', 'E');
+    g.addEdge('B', 'C');
+    g.addEdge('B', 'F');
+    g.addEdge('C', 'D');
+    g.addEdge('C', 'H');
+    g.addEdge('D', 'H');
+    g.addEdge('F', 'G');
+
+    return g;
+}
+
 describe('Graph data structure', function () {
     it('instantiates a graph instance with no parameters', function () {
         var graph = new Graph();
@@ -25,22 +64,21 @@ describe('Graph data structure', function () {
     });
 
     it('adds a vertex to the graph', function () {
-        var graph = new Graph();
-        graph.addVertex('A');
-        graph.addVertex('B');
-        expect(graph.vertices).to.have.length(2);
+        var graph = getSimpleGraph();
+        expect(graph.vertices).to.have.length(4);
     });
 
     it('adds an edge between vertices', function () {
-        var graph = new Graph();
-        graph.addVertex('A');
-        graph.addVertex('B');
-        graph.addVertex('C');
-        graph.addEdge('A', 'B');
-        graph.addEdge('A', 'C');
+        var graph = getSimpleGraph();
+
         expect(graph.adjacentList.get('A').indexOf('B') > -1).to.be.true;
         expect(graph.adjacentList.get('A').indexOf('C') > -1).to.be.true;
         expect(graph.adjacentList.get('B').indexOf('A') > -1).to.be.true;
+    });
+
+    it('does a breadth first search', function () {
+        var graph = getComplexGraph();
+        expect(graph.BFS('A')).to.be.eql(['A', 'B', 'E', 'C', 'F', 'D', 'H', 'G']);
     });
 
 });
